12ab294adaeaacc3639ec52a4dcaec094a149d2a
ryanj <ryanjrychlak@gmail.com>
2024-05-14 17:29:27 -0500

    fixed nav menu

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -1,3 +1,3 @@
-export const data = "";
+export const data = "h";
 
 export const title = 'Resume';
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,12 @@ import type { UserConfig } from 'vite';
 import UnoCSS from 'unocss/vite';
 
 const config: UserConfig = {
-	plugins: [UnoCSS(), sveltekit()]
+	plugins: [UnoCSS(), sveltekit()],
+	server: {
+		fs: {
+			allow: [".."]
+		}
+	}
 };
 
 export default config;
