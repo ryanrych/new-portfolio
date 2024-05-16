e1225d8da0e25501fce727d9517722ee40a662f0
ryanrych <46061503+ryanrych@users.noreply.github.com>
2024-05-16 19:00:16 +0000

    Deploying to gh-pages from @ ryanrych/new-portfolio@e1fae4488fa7943646404a0c3c5067eb5f790f3d 🚀

---
+++ _app/immutable/chunks/paths.9197f96b.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1aoljec)==null?void 0:s.base)??"/slick-portfolio-svelte";var e;const l=((e=globalThis.__sveltekit_1aoljec)==null?void 0:e.assets)??a;export{l as a,a as b};
